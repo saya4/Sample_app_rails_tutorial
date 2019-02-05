1d2429d3bbe8fee230a3948b11450be2592995b0
tsushima masaya <lemonlapis1@gmail.com>
2019-02-05 17:02:41 +0900

    Implement basic login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,13 +47,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  #config.force_ssl = true
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+ 
   
 end
